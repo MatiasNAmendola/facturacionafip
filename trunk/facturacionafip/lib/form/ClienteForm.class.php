131b21a81f647beee8b0c0871c11f65b5744de4c
ldgonza <ldgonza@8256818a-a3e7-11de-afd7-c7ca60a6e2ec>
2009-09-23 04:32:49 +0000

    Eliminados campos que no deberian ser visibles del formulario de edicion

--- trunk/facturacionafip/lib/form/ClienteForm.class.php
+++ trunk/facturacionafip/lib/form/ClienteForm.class.php
@@ -12,5 +12,8 @@ class ClienteForm extends BaseClienteForm
 {
   public function configure()
   {
+     // Deshacerse de la edicion del campo de baja logica y de las fechas de creacion y modificacion
+    unset($this['created_at'], $this['updated_at'], $this['activo']);
+
   }
 }

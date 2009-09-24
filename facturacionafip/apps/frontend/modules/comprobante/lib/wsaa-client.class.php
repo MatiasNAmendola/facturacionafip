b397310ce4224c8e36c4b235cdf49780ae698c05
abelfillol <abelfillol@8256818a-a3e7-11de-afd7-c7ca60a6e2ec>
2009-09-24 17:49:04 +0000

    No me anda la autenticación

---
+++ facturacionafip/apps/frontend/modules/comprobante/lib/wsaa-client.class.php
@@ -0,0 +1,113 @@
+<?php
+	# Author: Gerardo Fisanotti - DvSHyS/DiOPIN/AFIP - 13-apr-07
+# Function: Get an authorization ticket (TA) from AFIP WSAA
+# Input:
+#        WSDL, CERT, PRIVATEKEY, PASSPHRASE, SERVICE, WSAAURL
+#        Check below for its definitions
+# Output:
+#        TA.xml: the authorization ticket as granted by WSAA.
+#==============================================================================
+//define ("WSDL", "wsaa.wsdl");     # The WSDL corresponding to WSAA
+	sfConfig::set('WSDL', "wsaa.wsdl");
+//define ("CERT", "CUIT_20135344991.crt");       # The X.509 obtained from Seg. Inf.
+	sfConfig::set('CERT', "CUIT_20135344991.crt");
+//define ("PRIVATEKEY", "privada"); # The private key correspoding to CERT
+	sfConfig::set('PRIVATEKEY' ,"privada");
+//define ("PASSPHRASE", "xxxxxxx"); # The passphrase (if any) to sign
+	sfConfig::set('PASSPHRASE', "xxxxxxx");
+//# SERVICE: The WS service name you are asking a TA for
+//define ("SERVICE", "wsfe");
+	sfConfig::set('SERVICE', "wsfe");
+//# WSAAURL: the URL to access WSAA, check for http or https and wsaa or wsaahomo
+//#define ("WSAAURL", "https://wsaa.afip.gov.ar/ws/services/LoginCms");
+//define ("WSAAURL", "https://wsaahomo.afip.gov.ar/ws/services/LoginCms");
+	sfConfig::set('WSAAURL', "https://wsaahomo.afip.gov.ar/ws/services/LoginCms");
+//#------------------------------------------------------------------------------
+//# The following constants are related to remote web server verification
+//# If REMVERIFY is FALSE, then the other three constants don't matter.
+//define ("REMVERIFY",FALSE); # If FALSE, no remote Web Server verification done
+	sfConfig::set('REMVERIFY', FALSE);
+//define ("REMSELFSIGN",FALSE); # Do we accept self-signed certificates?
+	sfConfig::set('REMSELFSIGN', FALSE);
+//# Remote Web Server certificate's CN (CommonName)
+//define ("REMCN","wsaahomo.afip.gov.ar"); # WSAA homologacion
+	sfConfig::set('REMCN', "wsaahomo.afip.gov.ar");
+//# Remote Web Server CA Cert
+//define ("REMCACERT","AFIPcerthomo.crt"); # WSAA homologacion
+	sfConfig::set('REMCACERT', "AFIPcerthomo.crt"); # WSAA homologacion
+#------------------------------------------------------------------------------
+# You shouldn't have to change anything below this line!!!
+#==============================================================================
+
+	class WsaaClient {
+
+	public static function CreateTRA() {
+	  $TRA = new SimpleXMLElement(
+	    '<?xml version="1.0" encoding="UTF-8"?>' .
+	    '<loginTicketRequest version="1.0">'.
+	    '</loginTicketRequest>');
+	  $TRA->addChild('header');
+	  $TRA->header->addChild('uniqueId',date('U'));
+	  $TRA->header->addChild('generationTime',date('c',date('U')-600));
+	  $TRA->header->addChild('expirationTime',date('c',date('U')+600));
+	  $TRA->addChild('service',sfConfig::get('SERVICE'));
+	  $TRA->asXML('TRA.xml');
+	}
+	
+	#==============================================================================
+	# This functions makes the PKCS#7 signature using TRA as input file, CERT and
+	# PRIVATEKEY to sign. Generates an intermediate file and finally trims the 
+	# MIME heading leaving the final CMS required by WSAA.
+	public static function SignTRA() {
+		if (!file_exists(dirname(__FILE__).'/'.sfConfig::get('CERT'))) {exit("Failed to open ".dirname(__FILE__).'/'.sfConfig::get('CERT')."\n");}
+		if (!file_exists(dirname(__FILE__).'/'.sfConfig::get('PRIVATEKEY'))) {exit("Failed to open ".dirname(__FILE__).'/'.sfConfig::get('PRIVATEKEY')."\n");}
+		
+	  $STATUS=openssl_pkcs7_sign("TRA.xml", "TRA.tmp", dirname(__FILE__).'/'.sfConfig::get('CERT'),
+	    array(dirname(__FILE__).'/'.sfConfig::get('PRIVATEKEY'), sfConfig::get('PASSPHRASE')),
+	    array(),
+	    !PKCS7_DETACHED
+	    );
+	  if (!$STATUS) {exit("ERROR generating PKCS#7 signature\n"); var_dump(!$STATUS);}
+	  $inf=fopen("TRA.tmp", "r");
+	  $i=0;
+	  $CMS="";
+	  while (!feof($inf)) 
+	    { 
+	      $buffer=fgets($inf);
+	      if ( $i++ >= 4 ) {$CMS.=$buffer;}
+	    }
+	  fclose($inf);
+	  unlink("TRA.xml");
+	  unlink("TRA.tmp");
+	  return $CMS;
+	}
+	
+	#==============================================================================
+	public static function CallWSAA($CMS) {
+	  # Now we create a context to specify remote web server certificate checking
+	  # If you don want to check remote server, you may set verify_peer to FALSE.
+	  $ctx = stream_context_create( array('ssl'=>array(
+	    #'capath'            => "/xxx/yyy/",
+	    #'localcert'         => "crtPlusKey.pem",
+	    #'passphrase'        => "xxxx",
+	     'CN_match'          => sfConfig::get('REMCN'),
+	     'cafile'            => sfConfig::get('REMCACERT'),
+	     'allow_self_signed' => sfConfig::get('REMSELFSIGN'),
+	     'verify_peer'       => sfConfig::get('REMVERIFY')
+	     )));
+	  $client=new SoapClient(sfConfig::get('WSDL'), array(
+	          #'proxy_host'     => "proxy",
+	          #'proxy_port'     => 80,
+	          'stream_context' => $ctx,
+	          'soap_version'   => SOAP_1_2,
+	          'location'       => sfConfig::get('WSAAURL'),
+	          'exceptions'     => 0
+	          )); 
+	  $results=$client->loginCms(array('in0'=>$CMS));
+	  if (is_soap_fault($results)) {
+	  	exit("SOAP Fault: ".$results->faultcode."\n".$results->faultstring."\n");
+	  }
+	  return $results->loginCmsReturn;
+	}
+}
+?>
---
+++ facturacionafip/apps/frontend/modules/comprobante/lib/wsfe-client.class.php
@@ -0,0 +1,109 @@
+<?php
+class WsfeClient{
+	
+	public static function RecuperaQTY ($client, $token, $sign, $cuit) {
+	  $results=$client->FERecuperaQTYRequest(
+	    array('argAuth'=>array('Token' => $token,
+	                            'Sign' => $sign,
+	                            'cuit' => $cuit)));
+	  if ( $results->FERecuperaQTYRequestResult->RError->percode != 0 )
+	    {
+	      printf ("Percode: %d\nPerrmsg: %s\n", 
+	          $results->FERecuperaQTYRequestResult->RError->percode,
+	          $results->FERecuperaQTYRequestResult->RError->perrmsg);
+	      exit("Error");
+	    }
+	  return $results->FERecuperaQTYRequestResult->qty->value;
+	}
+	
+	#==============================================================================
+	public static function UltNro ($client, $token, $sign, $cuit) {
+	  $results=$client->FEUltNroRequest(
+	    array('argAuth'=>array('Token' => $token,
+	                            'Sign' => $sign,
+	                            'cuit' => $cuit)));
+	  if ( $results->FEUltNroRequestResult->RError->percode != 0 )
+	    {
+	      printf ("Percode: %d\nPerrmsg: %s\n", 
+	          $results->FEUltNroRequestResult->RError->percode,
+	          $results->FEUltNroRequestResult->RError->perrmsg);
+	      exit("Error");
+	    }
+	  return $results->FEUltNroRequestResult->nro->value;
+	}
+	
+	#==============================================================================
+	public static function RecuperaLastCMP ($client, $token, $sign, $cuit, $ptovta, $tipocbte){
+	  $results=$client->FERecuperaLastCMPRequest(
+	    array('argAuth' =>  array('Token'    => $token,
+	                              'Sign'     => $sign,
+	                              'cuit'     => $cuit),
+	           'argTCMP' => array('PtoVta'   => $ptovta,
+	                              'TipoCbte' => $tipocbte)));
+	  if ( $results->FERecuperaLastCMPRequestResult->RError->percode != 0 )
+	    {
+	      printf ("Percode: %d\nPerrmsg: %s\n", 
+	          $results->FERecuperaLastCMPRequestResult->RError->percode,
+	          $results->FERecuperaLastCMPRequestResult->RError->perrmsg);
+	      exit("Error");
+	    }
+	  return $results->FERecuperaLastCMPRequestResult->cbte_nro;
+	}
+	
+	#==============================================================================
+	public static function Aut ($client, $token, $sign, $cuit, $ID, $cbte) {
+	  $results=$client->FEAutRequest(
+	    array('argAuth' => array(
+	             'Token' => $token,
+	             'Sign'  => $sign,
+	             'cuit'  => $cuit),
+	          'Fer' => array(
+	             'Fecr' => array(
+	                'id' => $ID, 
+	                'cantidadreg' => 1, 
+	                'presta_serv' => 0),
+	             'Fedr' => array(
+	                'FEDetalleRequest' => array(
+	                   'tipo_doc' => 80,
+	                   'nro_doc' => 23111111113,
+	                   'tipo_cbte' => 1,
+	                   'punto_vta' => 1,
+	                   'cbt_desde' => $cbte,
+	                   'cbt_hasta' => $cbte,
+	                   'imp_total' => 121.0,
+	                   'imp_tot_conc' => 0,
+	                   'imp_neto' => 100.0,
+	                   'impto_liq' => 21.0,
+	                   'impto_liq_rni' => 0.0,
+	                   'imp_op_ex' => 0.0,
+	                   'fecha_cbte' => date('Ymd'),
+	                   'fecha_venc_pago' => date('Ymd'))))));
+	  if ( $results->FEAutRequestResult->RError->percode != 0 )
+	    {
+	      printf ("Percode: %d\nPerrmsg: %s\n", 
+	          $results->FEAutRequestResult->RError->percode,
+	          $results->FEAutRequestResult->RError->perrmsg);
+	      exit("Error");
+	    }
+	# printf ("HEADERs:\n%s\n", $client->__getLastRequestHeaders());
+	# printf ("REQUEST:\n%s\n", $client->__getLastRequest());
+	#  file_put_contents("FE.xml",$client->__getLastResponse());
+	  return $results->FEAutRequestResult->FedResp->FEDetalleResponse->cae;
+	}
+	
+	#==============================================================================
+	public static function dummy ($client) {
+	  $results=$client->FEDummy();
+	  printf("appserver status: %s\ndbserver status: %s\nauthserver status: %s\n",
+	         $results->FEDummyResult->appserver, 
+	         $results->FEDummyResult->dbserver, 
+	         $results->FEDummyResult->authserver);
+	  if (is_soap_fault($results)) 
+	   { printf("Fault: %s\nFaultString: %s\n",
+	             $results->faultcode, $results->faultstring); 
+	     exit (1);
+	   }
+	  return;
+	}
+}
+?>

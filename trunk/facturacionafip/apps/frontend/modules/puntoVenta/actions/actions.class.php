a445fbe69971a94f3f18e012423d2ebbd6a3e47e
abelfillol <abelfillol@8256818a-a3e7-11de-afd7-c7ca60a6e2ec>
2009-09-25 22:24:05 +0000

    Punto de venta ahora es una entidad mantenible

---
+++ trunk/facturacionafip/apps/frontend/modules/puntoVenta/actions/actions.class.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * puntoVenta actions.
+ *
+ * @package    facturacionafip
+ * @subpackage puntoVenta
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12474 2008-10-31 10:41:27Z fabien $
+ */
+class puntoVentaActions extends sfActions
+{
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->punto_venta_list = PuntoVentaPeer::doSelect(new Criteria());
+  }
+
+  public function executeShow(sfWebRequest $request)
+  {
+    $this->punto_venta = PuntoVentaPeer::retrieveByPk($request->getParameter('id'));
+    $this->forward404Unless($this->punto_venta);
+  }
+
+  public function executeNew(sfWebRequest $request)
+  {
+    $this->form = new PuntoVentaForm();
+  }
+
+  public function executeCreate(sfWebRequest $request)
+  {
+    $this->forward404Unless($request->isMethod('post'));
+
+    $this->form = new PuntoVentaForm();
+
+    $this->processForm($request, $this->form);
+
+    $this->setTemplate('new');
+  }
+
+  public function executeEdit(sfWebRequest $request)
+  {
+    $this->forward404Unless($punto_venta = PuntoVentaPeer::retrieveByPk($request->getParameter('id')), sprintf('Object punto_venta does not exist (%s).', $request->getParameter('id')));
+    $this->form = new PuntoVentaForm($punto_venta);
+  }
+
+  public function executeUpdate(sfWebRequest $request)
+  {
+    $this->forward404Unless($request->isMethod('post') || $request->isMethod('put'));
+    $this->forward404Unless($punto_venta = PuntoVentaPeer::retrieveByPk($request->getParameter('id')), sprintf('Object punto_venta does not exist (%s).', $request->getParameter('id')));
+    $this->form = new PuntoVentaForm($punto_venta);
+
+    $this->processForm($request, $this->form);
+
+    $this->setTemplate('edit');
+  }
+
+  public function executeDelete(sfWebRequest $request)
+  {
+    $request->checkCSRFProtection();
+
+    $this->forward404Unless($punto_venta = PuntoVentaPeer::retrieveByPk($request->getParameter('id')), sprintf('Object punto_venta does not exist (%s).', $request->getParameter('id')));
+    $punto_venta->delete();
+
+    $this->redirect('puntoVenta/index');
+  }
+
+  protected function processForm(sfWebRequest $request, sfForm $form)
+  {
+    $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
+    if ($form->isValid())
+    {
+      $punto_venta = $form->save();
+
+      $this->redirect('puntoVenta/edit?id='.$punto_venta->getId());
+    }
+  }
+}
---
+++ trunk/facturacionafip/apps/frontend/modules/puntoVenta/templates/showSuccess.php
@@ -0,0 +1,22 @@
+<table>
+  <tbody>
+    <tr>
+      <th>Id:</th>
+      <td><?php echo $punto_venta->getId() ?></td>
+    </tr>
+    <tr>
+      <th>Code:</th>
+      <td><?php echo $punto_venta->getCode() ?></td>
+    </tr>
+    <tr>
+      <th>Description:</th>
+      <td><?php echo $punto_venta->getDescription() ?></td>
+    </tr>
+  </tbody>
+</table>
+
+<hr />
+
+<a href="<?php echo url_for('puntoVenta/edit?id='.$punto_venta->getId()) ?>">Edit</a>
+&nbsp;
+<a href="<?php echo url_for('puntoVenta/index') ?>">List</a>

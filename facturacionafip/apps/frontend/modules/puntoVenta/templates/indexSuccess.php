1616f225444595fd2ca3afd69513ada1d78d2a30
abelfillol <abelfillol@8256818a-a3e7-11de-afd7-c7ca60a6e2ec>
2009-09-25 22:24:05 +0000

    Punto de venta ahora es una entidad mantenible

---
+++ facturacionafip/apps/frontend/modules/puntoVenta/templates/indexSuccess.php
@@ -0,0 +1,22 @@
+<h1>PuntoVenta List</h1>
+
+<table>
+  <thead>
+    <tr>
+      <th>Id</th>
+      <th>Code</th>
+      <th>Description</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($punto_venta_list as $punto_venta): ?>
+    <tr>
+      <td><a href="<?php echo url_for('puntoVenta/show?id='.$punto_venta->getId()) ?>"><?php echo $punto_venta->getId() ?></a></td>
+      <td><?php echo $punto_venta->getCode() ?></td>
+      <td><?php echo $punto_venta->getDescription() ?></td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+
+  <a href="<?php echo url_for('puntoVenta/new') ?>">New</a>

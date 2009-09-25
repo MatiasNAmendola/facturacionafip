300bd3a0424ccdd5c553fe58e6351313e8847840
ldgonza <ldgonza@8256818a-a3e7-11de-afd7-c7ca60a6e2ec>
2009-09-25 04:22:50 +0000

    Creada action de inicio

---
+++ trunk/facturacionafip/apps/frontend/modules/home/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Home Page actions.
+ *
+ * @package    facturacionafip
+ * @subpackage Home
+ * @author     ldgonza
+ * @version    SVN: $Id: actions.class.php 12474 2008-10-31 10:41:27Z fabien $
+ */
+class homeActions extends sfActions
+{
+  public function executeIndex(sfWebRequest $request)
+  {
+  }
+}

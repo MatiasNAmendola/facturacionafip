fa9d811ca6cfbb172b35357ec32d750708723591
ldgonza <ldgonza@8256818a-a3e7-11de-afd7-c7ca60a6e2ec>
2009-09-28 03:04:28 +0000

    cambios varios de presentacion

--- trunk/facturacionafip/apps/frontend/modules/cliente/templates/showSuccess.php
+++ trunk/facturacionafip/apps/frontend/modules/cliente/templates/showSuccess.php
@@ -6,33 +6,71 @@ echo 'Detalles del Cliente';
 
 <?php end_slot(); ?>
 
-<table>
-  <tbody>
-    <tr>
-      <th>Id:</th>
-      <td><?php echo $cliente->getId() ?></td>
-    </tr>
-    <tr>
-      <th>Razon social:</th>
-      <td><?php echo $cliente->getRazonSocial() ?></td>
-    </tr>
-    <tr>
-      <th>Tipo documento:</th>
-      <td><?php echo $cliente->getTipoDocumentoId() ?></td>
-    </tr>
-    <tr>
-      <th>Nro documento:</th>
-      <td><?php echo $cliente->getNroDocumento() ?></td>
-    </tr>
-    <tr>
-      <th>Direccion:</th>
-      <td><?php echo $cliente->getDireccion() ?></td>
-    </tr>
-  </tbody>
-</table>
+<div id="datosCliente" class="datosCliente" >
+    <table>
+      <tbody>
+	<tr>
+	  <th>Id:</th>
+	  <td><?php echo $cliente->getId() ?></td>
+	</tr>
+	<tr>
+	  <th>Razon social:</th>
+	  <td><?php echo $cliente->getRazonSocial() ?></td>
+	</tr>
+	<tr>
+	  <th>Tipo documento:</th>
+	  <td><?php echo $cliente->getTipoDocumentoId() ?></td>
+	</tr>
+	<tr>
+	  <th>Nro documento:</th>
+	  <td><?php echo $cliente->getNroDocumento() ?></td>
+	</tr>
+	<tr>
+	  <th>Direccion:</th>
+	  <td><?php echo $cliente->getDireccion() ?></td>
+	</tr>
+      </tbody>
+    </table>
 
-<hr />
+    <div id="linkBoton" class="linkBoton">
+        <a href="<?php echo url_for('cliente/edit?id='.$cliente->getId()) ?>">Editar Cliente</a>
+    </div>
+</div>
 
-<a href="<?php echo url_for('cliente/edit?id='.$cliente->getId()) ?>">Edit</a>
-&nbsp;
-<a href="<?php echo url_for('cliente/index') ?>">List</a>
+
+<div class="listaContactos" id="listaContactos">
+    <div class="subtitulo" id="subtituloListaContactos">
+      <h3>Contactos</h3>
+    </div>
+
+    <table>
+      <thead>
+	<tr>
+	  <th>Id</th>
+	  <th>Nombre</th>
+	  <th>Telefono</th>
+	  <th>E-Mail</th>
+	</tr>
+      </thead>
+      <tbody>
+	<?php foreach ($contactos as $contacto): ?>
+	<tr>
+	  <td><a href="<?php echo url_for('contacto/show?id='.$contacto->getId()) ?>"><?php echo $contacto->getId() ?></a></td>
+	  <td><?php echo $contacto->getNombre() ?></td>
+	  <td><?php echo $contacto->getTelefono() ?></td>
+	  <td><?php echo $contacto->getEmail() ?></td>
+	</tr>
+	<?php endforeach; ?>
+      </tbody>
+    </table>
+    <div id="linkBoton" class="linkBoton">
+      <a href="<?php echo url_for('contacto/new?cliente_id='.$cliente->getId()) ?>">Nuevo</a>
+    </div>
+
+</div>
+
+<div id="linkBoton" class="linkBoton">
+    <a href="<?php echo url_for('cliente/index') ?>">Volver</a>
+</div>
+
+

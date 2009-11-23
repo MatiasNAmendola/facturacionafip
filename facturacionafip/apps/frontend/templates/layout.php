767d62f59846a0381f0e529161b0904d4c879a2a
ldgonza <ldgonza@8256818a-a3e7-11de-afd7-c7ca60a6e2ec>
2009-11-23 04:32:00 +0000

    Inicio del refactor estetico; primeras versiones de cuadrados de bordes redondeados y botones; fondo con trama; se omitio toda consideracion de colores pero se deja todo preparado para que los colores sean triviales de aplicar; la aplicacion de efectos (sombras, brillos) puede requerir algo mas de trabajo

--- facturacionafip/apps/frontend/templates/layout.php
+++ facturacionafip/apps/frontend/templates/layout.php
@@ -6,31 +6,88 @@
     <?php include_title() ?>
     <link rel="shortcut icon" href="/favicon.ico" />
   </head>
-  <body>
+  <body id="body">
+
+
+       <div class="box">
+       <div class="cornerGray TL"></div>
+       <div class="cornerGray TR"></div>
+       <div class="cornerGray BL"></div>
+       <div class="cornerGray BR"></div>
+       <div class="cornerBoxInner gray">
+       <div class="borderFillerL gray"></div>
+       <div class="borderFillerR gray"></div>
+
     <div id="header" class="header">
        <p>
           <a href="<?php echo url_for('home/index') ?>">FACTURACION AFIP</a>
        <P>
     </div>
+
+</div>
+</div>
+
    
     <div id="menu" class="menu">
-   <div><a href="<?php echo url_for('cliente/index') ?>">Clientes</a></div>
-   <div><a href="<?php echo url_for('comprobante/index') ?>">Comprobantes</a></div>
-   <div><a href="<?php echo url_for('puntoVenta/index') ?>">Puntos de venta</a></div>
-    </div>
+   
+       <div class="box">
+       <div class="cornerGray TL"></div>
+       <div class="cornerGray TR"></div>
+       <div class="cornerGray BL"></div>
+       <div class="cornerGray BR"></div>
+       <div class="cornerBoxInner gray">
+       <div class="borderFillerL gray"></div>
+       <div class="borderFillerR gray"></div>
 
-    <div id="principal" class="principal"> 
-       <div id="titulo" class="titulo">
-           <h1>
-               <?php if (!include_slot('title')): ?>
-                    Bienvenido
-               <?php endif; ?>
-            </h1>
+
+
+
+  <span><a href="<?php echo url_for('cliente/index') ?>">Clientes</a></span>
+   <span> &nbsp;|&nbsp;  </span>
+   <span><a href="<?php echo url_for('comprobante/index') ?>">Comprobantes</a></span>
+   <span> &nbsp;|&nbsp;  </span>
+   <span><a href="<?php echo url_for('puntoVenta/index') ?>">Puntos de venta</a></
+
+span>
+
         </div>
-       <div id="contenido" class="contenido">
-         <?php echo $sf_content ?>
-       </div>
+
     </div>
+   <table class = "centered">
+   <tr>
+   <td>
+       <div class="box">
+       <div class="cornerGray TL"></div>
+       <div class="cornerGray TR"></div>
+       <div class="cornerGray BL"></div>
+       <div class="cornerGray BR"></div>
+
+
+       <div class="borderFillerL gray"></div>
+       <div class="borderFillerR gray"></div>
+
+       <div class="cornerBoxInner gray">
+
+
+            <div id="principal" class="principal"> 
+              <div id="titulo" class="titulo">
+                <h1>
+                  <?php if (!include_slot('title')): ?>
+                    Bienvenido
+                  <?php endif; ?>
+                </h1>
+              </div>
+              <div id="contenido" class="contenido">
+                <?php echo $sf_content ?> 
+              </div>
+            </div>
+   
+         </div>
+        </div>
+   </td>
+   </tr>
+   </table>
+
 
     <div id="separadorFinal" class="separadorFinal">
        <hr/>
